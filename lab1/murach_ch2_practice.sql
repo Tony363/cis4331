ab013e49171b4f9a1c32960c552cccb3566959f4
Tony363 <pysolver@gmail.com>
2024-01-19 19:11:27 -0500

    just running examples

---
+++ lab1/murach_ch2_practice.sql
@@ -0,0 +1,12 @@
+-- 7.
+-- select vendor_name from vendors;
+
+-- 8. 
+-- select vendor_name,vendor_address1,vendor_city,vendor_state,vendor_zip_code
+-- from vendors
+-- order by vendor_name
+
+--11.
+-- select count(*) as number_of_invoices,
+--     sum(invoice_total) as grand_invoice_total
+-- from invoices;
