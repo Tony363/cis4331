1a2db656cf01286876be9961cc194f65cb0325fe
Tony363 <pysolver@gmail.com>
2024-02-13 14:30:36 -0500

    in class exercise

--- Lec7-pkg/Lec7-pkg/Wk4-MurachCh4-InClassPractice.sql
+++ Lec7-pkg/Lec7-pkg/Wk4-MurachCh4-InClassPractice.sql
@@ -11,21 +11,57 @@
 --Print ids, names of students who took CS-101 or CS-190.
 --Must use a SQL set operator.
 
-
+select 
+    s.id, s.name
+from student s
+join takes t
+    on s.id = t.id
+where t.course_id = 'CS-101' 
+union
+select 
+    s.id,s.name
+from student s
+join takes t
+    on s.id = t.id
+where  t.course_id = 'CS-190'
 
 
 --8.
 --Print ids, names of students who took both CS-101 and CS-190.
 --Must use a SQL set operator.
   
-
+select 
+    s.id, s.name
+from student s
+join takes t
+    on s.id = t.id
+where t.course_id = 'CS-101' 
+intersect
+select 
+    s.id,s.name
+from student s
+join takes t
+    on s.id = t.id
+where  t.course_id = 'CS-190'
   
 --9.
 --print ids, names of students who took CS-101 but not CS-190.
 --Must use a SQL set operator.
 
  
-
+select 
+    s.id, s.name
+from student s
+join takes t
+    on s.id = t.id
+where t.course_id = 'CS-101' 
+minus
+select 
+    s.id,s.name
+from student s
+join takes t
+    on s.id = t.id
+where  t.course_id = 'CS-190'
   
 --10.
 --Use a SQL set operator to print names and states of all vendors.
@@ -42,10 +78,34 @@
 --......
 
 
-
+select 
+    v.vendor_name,
+    v.vendor_state
+from vendors v
+where v.vendor_state = 'CA'
+union
+select 
+    v1.vendor_name,
+    'Outside CA'vendor_state
+from vendors v1
+where v1.vendor_state != 'CA'
+order by vendor_state
 
 --11.
 --Rewrite the outer join in Question 6 using a SQL set operator.
 
 
-
+-- all gla accounts
+-- only gla accounts use d in any accounts
+select 
+    gla.account_number, 
+    -- account_description
+from general_ledger_accounts gla 
+union
+select 
+    il.account_number
+from invoice_line_items il
+where il.account_number is null;
+-- left outer join invoice_line_items il
+--     on (gla.account_number = il.account_number)
+-- where il.account_number is null;
---
+++ Lec9-Pkg/Lec9-Pkg/MurachCh5-InClassPractice.sql
@@ -0,0 +1,82 @@
+--Week 5, Murach Ch5 in class practice.
+
+--Covered topics: GROUP BY, HAVING, WHERE in summary queries
+
+--Run all queries as user AP
+
+SELECT TABLE_NAME
+FROM ALL_TAB_COLUMNS
+WHERE COLUMN_NAME = 'VENDOR_ID';
+
+
+--1.
+--For each vendor that has at least one invoice, print its vendor id, 
+--   and total invoice amount of all invoices for this vendor.
+select 
+    v.vendor_id,
+    sum(i.invoice_total) as total_invoice_amount
+from vendors v
+inner join invoices i
+    on v.vendor_id = i.vendor_id
+group by v.vendor_id
+having count(i.VENDOR_ID) > 0
+
+
+
+
+
+--2.
+--For each vendor that has at least one invoice, print its name, 
+--  the total payment amount of all its invoices.
+--Sort the result in decreasing order of the total payment amount.
+select 
+    v.vendor_id,
+    v.vendor_name,
+    sum(i.invoice_total) as "total_invoice_amount"
+from vendors v
+inner join invoices i
+    on v.vendor_id = i.vendor_id
+group by v.vendor_id,v.vendor_name
+order by  sum(i.invoice_total) desc
+
+
+--3.
+--For each vendor that has at least one invoice, print its name, 
+--   invoice quantity, and total invoice amount of all its invoices.
+--Sort the query result so that the vendor with most invoices appear first.
+--Display headings INVOICE_COUNT, TOTAL_INVOICE_AMT for invoice quantiy, 
+--  and total invoice amount.
+
+select 
+    v.vendor_name,
+    count(i.invoice_id) as "invoice_count",
+    sum(i.invoice_total) as "total_invoice_amount"
+from vendors v
+inner join invoices i
+    on v.vendor_id = i.vendor_id
+group by v.vendor_name
+order by count(i.invoice_id) desc
+
+
+
+--4.
+--For each general ledger account that is used by at least 2 invoice items, 
+--  print its account description, how many invoice items use this account,
+--  and the total item amount of all these items.
+--Sort the result so that the account with the largest total item amount appears first.
+--Display headins LINE_ITEM_COUNT, TOTAL_LINE_ITEM_AMT for invoice item quantity, 
+--   and total item amount.
+
+
+select 
+    gla.account_description,
+    count(ii.account_number) as "line_item_count",
+    sum(ii.line_item_amt) as "total_line_item_amount"
+from general_ledger_accounts gla
+inner join invoice_line_items ii
+    on gla.account_number = ii.account_number
+group by gla.account_description
+having count(ii.account_number) > 1
+order by sum(ii.line_item_amt) desc
+
+
